544c9129f44c3f0cf7d9828458b66be5f6277b18
pearisgreen <gerrit.proessl@gmail.com>
2019-06-08 19:24:29 +0200

    . Fix read image

--- src/fileio.asm
+++ src/fileio.asm
@@ -3,7 +3,7 @@
 
   .data
 
-buffer db 200 dup (0)
+buffer db 200 dup ('$')
 
 fpath db "test.txt$", 0
 
@@ -16,46 +16,80 @@ fpath db "test.txt$", 0
   extrn error:proc
   extrn print:proc
 
+  ;; reads and prints contents of a file
 read_bmp proc near
-l_fh = [bp]
-l_fs = [bp + 2]
-  enter 4, 0
+  enter 0, 0
 
-  mov dx, offset fpath
+  mov ax, @data
+  mov ds, ax
 
-  ;; open file and save handle to ax
-  ;; exit on error
-  mov ah, 3dh
-  mov ax, 0                     ; reading only
-  int 21h
-  jc l_open_err
-
-  mov dx, offset buffer         ; desination
-l_read_byte:
-  ;; read from file
-  mov ah, 3fh
-  mov cx, 1                     ; 1 byte
-  mov bx, l_fh
-  int 21h
-  jc l_read_err
-
-  inc dx
-  cmp ax, cx
-  je l_read_byte
+  push offset fpath
+  push offset buffer
+  push 200
+  call read_file
+  add sp, 6
 
   ;; eof
-  mov [buffer], 24h
+  mov bx, dx
+  mov [bx], 24h
 
   ;; print content of file
   mov ax, offset buffer
   call print
 
+  leave
+  ret
+  endp
+
+
+  ;; reads file from disk
+  ;; args:
+  ;;  path - pointer to filename
+  ;;  buf -  pointer to buffer
+  ;;  len - length of buffer
+read_file proc near
+path = [bp + 4 + 4]
+buf = [bp + 4 + 2]
+len = [bp + 4]
+fhandle = [bp]
+  enter 2, 0
+
+  mov ax, @data
+  mov ds, ax
+
+  mov dx, path
+
+  ;; open file and save handle to ax
+  ;; exit on error
+  mov ah, 3dh
+  mov al, 0                     ; reading only
+  int 21h
+  jc l_open_err
+
+  mov fhandle, ax
+
+  mov dx, buf                   ; desination
+l_read_byte:
+  cmp len, 0
+  je l_cleanup
+  ;; read from file
+  mov ah, 3fh
+  mov cx, 1                     ; 1 byte
+  mov bx, fhandle
+  int 21h
+  jc l_read_err
+
+  dec len
+  inc dx
+  cmp ax, cx
+  je l_read_byte
+
+l_cleanup:
   ;; close file
-  mov bx, l_fh
+  mov bx, fhandle
   mov ah, 3eh
   int 21h
   jc l_close_err
-
 
   leave
   ret
@@ -65,7 +99,7 @@ l_open_err:
   call error
 l_read_err:
   ;; close file
-  mov bx, l_fh
+  mov bx, fhandle
   mov ah, 3eh
   int 21h
   jc l_close_err
